b6335a9974d7de2fa71549cbf511d4e8a93f8627
ABDULLAH Muhammad <muhammad.abdullah@opg.com>
2024-06-28 13:15:26 -0400

    initial frontend setup and routes

--- frontend/class-manager/src/app/app.routes.ts
+++ frontend/class-manager/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainPageComponent } from './components/pages/main-page/main-page.compon
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { SignupPageComponent } from './components/pages/signup-page/signup-page.component';
 
 export const routes: Routes = [
     {
@@ -25,5 +26,9 @@ export const routes: Routes = [
     {
         path: 'login-page',
         component: LoginPageComponent
+    },
+    {
+        path: 'signup-page',
+        component: SignupPageComponent
     }
 ];
